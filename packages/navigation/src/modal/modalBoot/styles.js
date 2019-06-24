48b70faed3d64caf4337b346f3bf6dca04aed0c6
hossein mohammadi <hosseinm.developer@gmail.com>
2019-06-24 18:15:06 +0430

    fix ios bug

--- packages/navigation/src/modal/modalBoot/styles.js
+++ packages/navigation/src/modal/modalBoot/styles.js
@@ -1,13 +1,8 @@
 import { StyleSheet } from "react-native";
-import config from "../../config"; //  using now visual code bug
-import { platform } from "@react-cross/utility";
 export default StyleSheet.create({
   modal_container: {
     overflow: "hidden",
     ...StyleSheet.absoluteFillObject,
-    ...(platform.isiOS
-      ? { paddingTop: config.isAppleStatusBarBig ? 41 : 20 }
-      : { paddingTop: 0 })
   },
   ModalStatusBar: {
     position: "absolute",
