3149387a3cc41b24ff304b08b52d0c4d88a3da21
hossein mohammadi <hosseinm.developer@gmail.com>
2019-06-25 12:30:29 +0430

    v 0.2.2

--- packages/navigation/src/slider/styles.js
+++ packages/navigation/src/slider/styles.js
@@ -1,11 +1,10 @@
 import { StyleSheet } from "react-native";
 import shadows from "../common/shadows";
-import { dimensions } from "../common";
 export default StyleSheet.create({
   content_container: {
     ...shadows.menu,
     position: "absolute",
-    width: dimensions.width - 20,
+    width: "95%",
     borderRadius: 20,
     margin: 10,
     overflow: "hidden",
