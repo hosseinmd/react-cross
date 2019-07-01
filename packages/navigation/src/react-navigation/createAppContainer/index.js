3e92a7b274d938cfed5661087a2eff530fb5211d
hossein mohammadi <hosseinm.developer@gmail.com>
2019-07-01 18:00:34 +0430

    remove modal from createApp

--- packages/navigation/src/react-navigation/createAppContainer/index.js
+++ packages/navigation/src/react-navigation/createAppContainer/index.js
@@ -2,12 +2,9 @@ import { createAppContainer as RNcreateAppContainer } from "./mainSource";
 import { getGlobal } from "../../logic/store";
 import Navigator from "../../navigator";
 import config from "../../config";
-export default function createAppContainer(
-  Component,
-  { modals = {}, modals_statusbar = {} },
-) {
+export default function createAppContainer(Component) {
   const appContainer = RNcreateAppContainer(Component);
   const [, actions] = getGlobal();
-  actions.setAppContainer({ appContainer, modals, modals_statusbar });
+  actions.setAppContainer({ appContainer });
   return Navigator;
 }
