9604085c330a8c013fc4eec5821b4dcda0606621
hossein mohammadi <hosseinm.developer@gmail.com>
2019-07-06 10:56:51 +0430

    fix responsive problem in web

--- packages/navigation/src/navigator/styles.js
+++ packages/navigation/src/navigator/styles.js
@@ -1,9 +1,19 @@
 import { StyleSheet } from "react-native";
-import { dimensions } from "@react-cross/utility";
+import { platform } from "@react-cross/utility";
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    height: dimensions.height,
+    ...platform.select({
+      web: {
+        position: "fixed",
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      },
+      default: {
+        flex: 1,
+      },
+    }),
     backgroundColor: "#fff",
   },
 });
