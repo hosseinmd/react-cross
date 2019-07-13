fcbe9ea8733240640835709c1976ffd3936efdd9
hossein mohammadi <hosseinm.developer@gmail.com>
2019-07-13 17:05:44 +0430

    add throttle for save navigation

--- packages/navigation/src/action/index.js
+++ packages/navigation/src/action/index.js
@@ -17,8 +17,26 @@ export const action = {
    * @typedef NavigationScreen
    * @type {import("../react-navigation").NavigationScreenProp<import("../react-navigation").NavigationState>}
    */
+
   /**@type {NavigationScreen} */
   navigation: null,
+
+  /**
+   * @private
+   */
+  _navigationIsEnable: true,
+
+  throttle(func) {
+    return function() {
+      if (action._navigationIsEnable) {
+        action._navigationIsEnable = false;
+        func(arguments);
+        setTimeout(function() {
+          action._navigationIsEnable = true;
+        }, 500);
+      }
+    };
+  },
   /**
    * @param {NavigationScreen} navigation
    * @returns {boolean}
@@ -108,53 +126,6 @@ export const action = {
     },
   },
 
-  modal: {
-    list() {
-      return Object.values(_state.modal);
-    },
-    set(name, props = {}) {
-      const id = new Date().getTime();
-      _state.modal[id] = {
-        id,
-        name,
-        props,
-      };
-      emit(enums.JETEMIT.MODAL, this.list());
-    },
-    get(id) {
-      if (id) return _state.modal[id];
-      else return _state.modal[this.getLastIndex()];
-    },
-    getLastIndex() {
-      let keys = Object.keys(_state.modal);
-      return keys[keys.length - 1];
-    },
-    props: {
-      get: id => {
-        return action.modal.get(id).props;
-      },
-      set: props => {
-        action.modal.get().props = Object.assign(
-          { ...action.modal.get().props },
-          props,
-        );
-        emit(enums.JETEMIT.MODAL_PROPS, action.modal.get().props);
-      },
-      willChange(callback) {
-        return on(enums.JETEMIT.MODAL_PROPS, callback);
-      },
-    },
-    hide(id) {
-      id
-        ? emit(enums.JETEMIT.MODAL_HIDE, id)
-        : emit(enums.JETEMIT.MODAL_HIDE, this.getLastIndex());
-    },
-    //please use hide function
-    remove(id) {
-      id ? delete _state.modal[id] : delete _state.modal[this.getLastIndex()];
-      emit(enums.JETEMIT.MODAL, this.list());
-    },
-  },
   message: {
     TYPE_ERROR: "error",
     TYPE_WARNING: "warning",
