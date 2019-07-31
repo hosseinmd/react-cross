2e7f949ca8d07e84682dbaa09e0b9e5fb2c1e2a2
Hossein Mohammadi <hotman.hh@gmail.com>
2019-07-31 12:20:42 +0430

    add createContainer

--- packages/navigation/src/react-navigation/index.js
+++ packages/navigation/src/react-navigation/index.js
@@ -3,183 +3,186 @@ import "react-native-gesture-handler"; // fix bug
 module.exports = {
   // Native
   get createAppContainer() {
-    return require('./createAppContainer').default;
+    return require("./createAppContainer").default;
+  },
+  get createContainer() {
+    return require("./createAppContainer").createContainer;
   },
   get createKeyboardAwareNavigator() {
-    return require('@react-navigation/native').createKeyboardAwareNavigator;
+    return require("@react-navigation/native").createKeyboardAwareNavigator;
   },
   get createNavigationAwareScrollable() {
-    return require('@react-navigation/native').createNavigationAwareScrollable;
+    return require("@react-navigation/native").createNavigationAwareScrollable;
   },
   get ScrollView() {
-    return require('@react-navigation/native').ScrollView;
+    return require("@react-navigation/native").ScrollView;
   },
   get FlatList() {
-    return require('@react-navigation/native').FlatList;
+    return require("@react-navigation/native").FlatList;
   },
   get SectionList() {
-    return require('@react-navigation/native').SectionList;
+    return require("@react-navigation/native").SectionList;
   },
   get ResourceSavingSceneView() {
-    return require('@react-navigation/native').ResourceSavingSceneView;
+    return require("@react-navigation/native").ResourceSavingSceneView;
   },
   get SafeAreaView() {
-    return require('@react-navigation/native').SafeAreaView;
+    return require("@react-navigation/native").SafeAreaView;
   },
   get withOrientation() {
-    return require('@react-navigation/native').withOrientation;
+    return require("@react-navigation/native").withOrientation;
   },
 
   // Core
   get createNavigator() {
-    return require('@react-navigation/core').createNavigator;
+    return require("@react-navigation/core").createNavigator;
   },
   get StateUtils() {
-    return require('@react-navigation/core').StateUtils;
+    return require("@react-navigation/core").StateUtils;
   },
   get getNavigation() {
-    return require('@react-navigation/core').getNavigation;
+    return require("@react-navigation/core").getNavigation;
   },
   get NavigationContext() {
-    return require('@react-navigation/core').NavigationContext;
+    return require("@react-navigation/core").NavigationContext;
   },
   get NavigationProvider() {
-    return require('@react-navigation/core').NavigationProvider;
+    return require("@react-navigation/core").NavigationProvider;
   },
   get NavigationConsumer() {
-    return require('@react-navigation/core').NavigationConsumer;
+    return require("@react-navigation/core").NavigationConsumer;
   },
   get NavigationActions() {
-    return require('@react-navigation/core').NavigationActions;
+    return require("@react-navigation/core").NavigationActions;
   },
   get StackActions() {
-    return require('@react-navigation/core').StackActions;
+    return require("@react-navigation/core").StackActions;
   },
   get StackRouter() {
-    return require('@react-navigation/core').StackRouter;
+    return require("@react-navigation/core").StackRouter;
   },
   get TabRouter() {
-    return require('@react-navigation/core').TabRouter;
+    return require("@react-navigation/core").TabRouter;
   },
   get SwitchRouter() {
-    return require('@react-navigation/core').SwitchRouter;
+    return require("@react-navigation/core").SwitchRouter;
   },
   get createConfigGetter() {
-    return require('@react-navigation/core').StackAcreateConfigGetterctions;
+    return require("@react-navigation/core").StackAcreateConfigGetterctions;
   },
   get getScreenForRouteName() {
-    return require('@react-navigation/core').getScreenForRouteName;
+    return require("@react-navigation/core").getScreenForRouteName;
   },
   get validateRouteConfigMap() {
-    return require('@react-navigation/core').validateRouteConfigMap;
+    return require("@react-navigation/core").validateRouteConfigMap;
   },
   get getActiveChildNavigationOptions() {
-    return require('@react-navigation/core').getActiveChildNavigationOptions;
+    return require("@react-navigation/core").getActiveChildNavigationOptions;
   },
   get pathUtils() {
-    return require('@react-navigation/core').pathUtils;
+    return require("@react-navigation/core").pathUtils;
   },
   get SceneView() {
-    return require('@react-navigation/core').SceneView;
+    return require("@react-navigation/core").SceneView;
   },
   get SwitchView() {
-    return require('@react-navigation/core').SwitchView;
+    return require("@react-navigation/core").SwitchView;
   },
   get NavigationEvents() {
-    return require('@react-navigation/core').NavigationEvents;
+    return require("@react-navigation/core").NavigationEvents;
   },
   get withNavigation() {
-    return require('@react-navigation/core').withNavigation;
+    return require("@react-navigation/core").withNavigation;
   },
   get withNavigationFocus() {
-    return require('@react-navigation/core').withNavigationFocus;
+    return require("@react-navigation/core").withNavigationFocus;
   },
 
   // Navigators
 
   get createStackNavigator() {
-    return require('react-navigation-stack').createStackNavigator;
+    return require("react-navigation-stack").createStackNavigator;
   },
   get createSwitchNavigator() {
-    return require('@react-navigation/core').createSwitchNavigator;
+    return require("@react-navigation/core").createSwitchNavigator;
   },
 
   get createBottomTabNavigator() {
-    return require('react-navigation-tabs').createBottomTabNavigator;
+    return require("react-navigation-tabs").createBottomTabNavigator;
   },
   get createMaterialTopTabNavigator() {
-    return require('react-navigation-tabs').createMaterialTopTabNavigator;
+    return require("react-navigation-tabs").createMaterialTopTabNavigator;
   },
 
   get createDrawerNavigator() {
-    return require('react-navigation-drawer').createDrawerNavigator;
+    return require("react-navigation-drawer").createDrawerNavigator;
   },
 
   // Gesture contexts
 
   get StackGestureContext() {
-    return require('react-navigation-stack').StackGestureContext;
+    return require("react-navigation-stack").StackGestureContext;
   },
   get DrawerGestureContext() {
-    return require('react-navigation-drawer').DrawerGestureContext;
+    return require("react-navigation-drawer").DrawerGestureContext;
   },
 
   // Routers and Actions
 
   get DrawerRouter() {
-    return require('react-navigation-drawer').DrawerRouter;
+    return require("react-navigation-drawer").DrawerRouter;
   },
   get DrawerActions() {
-    return require('react-navigation-drawer').DrawerActions;
+    return require("react-navigation-drawer").DrawerActions;
   },
 
   // Views
   get Transitioner() {
     console.warn(
-      'Importing the stack Transitioner directly from react-navigation is now deprecated. Instead, import { Transitioner } from "react-navigation-stack";'
+      'Importing the stack Transitioner directly from react-navigation is now deprecated. Instead, import { Transitioner } from "react-navigation-stack";',
     );
-    return require('react-navigation-stack').Transitioner;
+    return require("react-navigation-stack").Transitioner;
   },
   get StackView() {
-    return require('react-navigation-stack').StackView;
+    return require("react-navigation-stack").StackView;
   },
   get StackViewCard() {
-    return require('react-navigation-stack').StackViewCard;
+    return require("react-navigation-stack").StackViewCard;
   },
   get StackViewTransitionConfigs() {
-    return require('react-navigation-stack').StackViewTransitionConfigs;
+    return require("react-navigation-stack").StackViewTransitionConfigs;
   },
 
   // Header
   get Header() {
-    return require('react-navigation-stack').Header;
+    return require("react-navigation-stack").Header;
   },
   get HeaderTitle() {
-    return require('react-navigation-stack').HeaderTitle;
+    return require("react-navigation-stack").HeaderTitle;
   },
   get HeaderBackButton() {
-    return require('react-navigation-stack').HeaderBackButton;
+    return require("react-navigation-stack").HeaderBackButton;
   },
   get HeaderStyleInterpolator() {
-    return require('react-navigation-stack').HeaderStyleInterpolator;
+    return require("react-navigation-stack").HeaderStyleInterpolator;
   },
 
   // DrawerView
   get DrawerView() {
-    return require('react-navigation-drawer').DrawerView;
+    return require("react-navigation-drawer").DrawerView;
   },
   get DrawerItems() {
-    return require('react-navigation-drawer').DrawerNavigatorItems;
+    return require("react-navigation-drawer").DrawerNavigatorItems;
   },
   get DrawerSidebar() {
-    return require('react-navigation-drawer').DrawerSidebar;
+    return require("react-navigation-drawer").DrawerSidebar;
   },
 
   // Tabs
   get BottomTabBar() {
-    return require('react-navigation-tabs').BottomTabBar;
+    return require("react-navigation-tabs").BottomTabBar;
   },
   get MaterialTopTabBar() {
-    return require('react-navigation-tabs').MaterialTopTabBar;
+    return require("react-navigation-tabs").MaterialTopTabBar;
   },
 };
