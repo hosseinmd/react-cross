ae50fe9c24b33f9c797fb8391699873b49237a65
Hossein Mohammadi <hotman.hh@gmail.com>
2019-07-31 12:22:12 +0430

    delete typography

--- packages/utility/src/index.js
+++ packages/utility/src/index.js
@@ -1,6 +1,5 @@
 export { dimensions } from "./dimensions";
 export { platform } from "./platform";
-export { typography } from "./typography";
 export { shadows } from "./shadows";
 export { helper } from "./helper";
 
